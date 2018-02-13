b3a51127c9e0b17ad7082f807fe6b1705783adcf
Mehuge <adf@sorcerer.co.uk>
2018-02-13 22:57:32 +0000

    Fix tokeniser line count

---
+++ tests/tokeniser.test.js
@@ -0,0 +1,44 @@
+const { StringTokeniser } = require('../tokeniser');
+
+// note, must leave tabs and trailing spaces
+const script = `
+L2 hello world
+L3 /* 1000 1.2 */ 
+L4 -1000 -01.20 
+L5	100,100 
+L6	abc"123" /* humm */
+L7	abc""123 /* abc123 */
+L8	123abc /* is a string */
+	abc123 /* is a string */
+	abc-123 /* is a string */
+	// ignore this line
+	"hello,there"
+	/* hello */
+`;
+
+test('Tokeniser', () => {
+
+  const tokeniser = new StringTokeniser(script, { wordChars: /[A-Za-z0-9$#_\-]/ });
+
+  expect(tokeniser.next()).toEqual({ token: 'L2', type: 1, nextch: ' ', lineno: 2 });
+  expect(tokeniser.next()).toEqual({ token: 'hello', type: 1, nextch: ' ', lineno: 2 });
+  expect(tokeniser.next()).toEqual({ token: 'world', type: 1, nextch: '\n', lineno: 2 });
+  expect(tokeniser.next()).toEqual({ token: 'L3', type: 1, nextch: ' ', lineno: 3 });
+  expect(tokeniser.next()).toEqual({ token: 'L4', type: 1, nextch: ' ', lineno: 4 });
+  expect(tokeniser.next()).toEqual({ token: -1000, type: 0, nextch: ' ', lineno: 4 });
+  expect(tokeniser.next()).toEqual({ token: -1.2, type: 0, nextch: ' ', lineno: 4 });
+  expect(tokeniser.next()).toEqual({ token: 'L5', type: 1, nextch: '\t', lineno: 5 });
+  expect(tokeniser.next()).toEqual({ token: 100, type: 0, nextch: ',', lineno: 5 });
+  expect(tokeniser.next()).toEqual({ token: ',', type: 2, nextch: '1', lineno: 5 });
+  expect(tokeniser.next()).toEqual({ token: 100, type: 0, nextch: ' ', lineno: 5 });
+  expect(tokeniser.next()).toEqual({ token: 'L6', type: 1, nextch: '\t', lineno: 6 });
+  expect(tokeniser.next()).toEqual({ token: 'abc123', type: 1, nextch: ' ', lineno: 6 });
+  expect(tokeniser.next()).toEqual({ token: 'L7', type: 1, nextch: '\t', lineno: 7 });
+  expect(tokeniser.next()).toEqual({ token: 'abc123', type: 1, nextch: ' ', lineno: 7 });
+  expect(tokeniser.next()).toEqual({ token: 'L8', type: 1, nextch: '\t', lineno: 8 });
+  expect(tokeniser.next()).toEqual({ token: '123abc', type: 1, nextch: ' ', lineno: 8 });
+  expect(tokeniser.next()).toEqual({ token: 'abc123', type: 1, nextch: ' ', lineno: 9 });
+  expect(tokeniser.next()).toEqual({ token: 'abc-123', type: 1, nextch: ' ', lineno: 10 });
+  expect(tokeniser.next()).toEqual({ token: 'hello,there', type: 1, nextch: '\n', lineno: 12 });
+  expect(tokeniser.next()).toEqual({ token: '', type: -1, nextch: undefined, lineno: 14 });
+});
--- tokeniser.js
+++ tokeniser.js
@@ -7,142 +7,141 @@ const SYMBOL = 2;
 const SPACE = 3;
 
 class Lexicaliser {
-	constructor(options) {
-		this.options = options;
-	}
-	type(ch,pos) {
-		const { wordChars, whiteSpace } = this.options;
-		if (!ch) return EOF;
-		if (ch.match(/[0-9.]/)) return NUMBER;
-		if (pos === 0 && ch === '-') return NUMBER;
-		if (ch.match(wordChars)) return STRING;
-		if (ch.match(whiteSpace)) return SPACE;
-		return SYMBOL;
-	}
-	word(ch) {
-		return ch.match(this.options.wordChars);
-	}
+  constructor(options) {
+    this.options = options;
+  }
+  type(ch,pos) {
+    const { wordChars, whiteSpace } = this.options;
+    if (!ch) return EOF;
+    if (ch.match(/[0-9.]/)) return NUMBER;
+    if (pos === 0 && ch === '-') return NUMBER;
+    if (ch.match(wordChars)) return STRING;
+    if (ch.match(whiteSpace)) return SPACE;
+    return SYMBOL;
+  }
+  word(ch) {
+    return ch.match(this.options.wordChars);
+  }
 }
 
 class Tokeniser {
-	constructor({ 
-		quoteChar = '"', 
-		slashStarComments = true, 
-		slashSlashComments = true, 
-		whiteSpace = /[ 	\n]/, 
-		wordChars = /[A-Za-z0-9$#_\-]/
-	} = {}) {
-		this.options = { qc: quoteChar, sstc: slashStarComments, sslc: slashSlashComments };
-		this.lexicaliser = new Lexicaliser({ whiteSpace, wordChars });
-		this.lineno = 1;
-	}
-	getch() {
-		if (this.lastch === "\n") this.lineno ++;
-		this.lastch = this.get();
-		return this.lastch;
-	}
-	next() {
-		const { wordChars, qc, sstc, sslc } = this.options;
-		const l = this.lexicaliser;
-		let ch = this.nextch === undefined ? this.getch() : this.nextch;
-		let token = this.token = '';
-		let nextch = this.nextch = undefined;
-		let type = this.type = EOF;
-		while (l.type(ch) === SPACE) {			// consume white space
-			ch = this.getch();
-		}
-		while (ch !== undefined) {
-			const chtype = l.type(ch, token.length);
-			if ((type === EOF||type === NUMBER) && chtype === NUMBER) {
-				token += ""+ch;
-				type = NUMBER;
-			} else if (chtype === STRING || l.word(ch)) {
-				token += ch;
-				type = STRING;
-			} else if (chtype === SPACE) {
-				nextch = ch;
-				break;
-			} else {
-				// symbol of some kind except quote
-				if (token.length && ch !== qc) {
-					nextch = ch;
-					break;
-				} else {
-					nextch = this.getch();
-					if (ch === qc) {			// start of quoted string
-						type = STRING;
-						if (nextch === qc) {
-							nextch = this.getch();
-						} else {
-							token += nextch;
-							while (ch = this.getch()) {
-								if (ch === qc) {
-									nextch = this.getch();
-									break;
-								}
-								token += ch;
-							}
-						}
-						if (nextch && l.word(nextch)) {
-							ch = nextch;
-							nextch = undefined;
-							continue;
-						}
-					} else if (ch === '/') { 
-						if (sslc && nextch === '/') {
-							// consume // comments
-							while (ch = this.get()) {
-								if (ch === "\n") {
-									this.nextch = ch;
-									break;
-								}
-							}
-							if (ch) return this.next();
-							token = '';
-							type = EOF;
-						} else if (sstc && nextch === '*') {
-							// consume /* ... */ comments
-							while (ch = this.get()) {
-								if (ch === "*") {
-									ch = this.get();
-									if (ch === '/') {
-										break;
-									}
-								}
-								if (ch === "\n") this.lineno++;
-							}
-							if (ch) return this.next();
-							token = '';
-							type = EOF;
-						}
-					} else {
-						token = ch;
-						type = SYMBOL;
-					}
-					break;
-				}
-			}
-			ch = this.get();
-		}
-		this.token = token;
-		this.type = type;
-		this.nextch = nextch;
-		if (type === NUMBER) {
-			token = parseFloat(token);
-		}
-		return { token, type, nextch, lineno: this.lineno };
-	}
+  constructor({
+    quoteChar = '"',
+    slashStarComments = true,
+    slashSlashComments = true,
+    whiteSpace = /[ 	\n]/,
+    wordChars = /[A-Za-z0-9$#_\-]/
+  } = {}) {
+    this.options = { qc: quoteChar, sstc: slashStarComments, sslc: slashSlashComments };
+    this.lexicaliser = new Lexicaliser({ whiteSpace, wordChars });
+    this.lineno = 1;
+  }
+  getch() {
+    if (this.lastch === "\n") this.lineno ++;
+    this.lastch = this.get();
+    return this.lastch;
+  }
+  next() {
+    const { wordChars, qc, sstc, sslc } = this.options;
+    const l = this.lexicaliser;
+    let ch = this.nextch === undefined ? this.getch() : this.nextch;
+    let token = this.token = '';
+    let nextch = this.nextch = undefined;
+    let type = this.type = EOF;
+    while (l.type(ch) === SPACE) {			// consume white space
+      ch = this.getch();
+    }
+    while (ch !== undefined) {
+      const chtype = l.type(ch, token.length);
+      if ((type === EOF||type === NUMBER) && chtype === NUMBER) {
+        token += ""+ch;
+        type = NUMBER;
+      } else if (chtype === STRING || l.word(ch)) {
+        token += ch;
+        type = STRING;
+      } else if (chtype === SPACE) {
+        nextch = ch;
+        break;
+      } else {
+        if (token.length && ch !== qc) {
+          // a symbol terminate an existing token (unless its a quote)
+          nextch = ch;
+          break;
+        }
+        nextch = this.getch();
+        if (ch === qc) {			// start of quoted string
+          type = STRING;
+          if (nextch === qc) {
+            nextch = this.getch();
+          } else {
+            token += nextch;
+            while (ch = this.getch()) {
+              if (ch === qc) {
+                nextch = this.getch();
+                break;
+              }
+              token += ch;
+            }
+          }
+          if (nextch && l.word(nextch)) {
+            ch = nextch;
+            nextch = undefined;
+            continue;
+          }
+        } else if (ch === '/') {
+          if (sslc && nextch === '/') {
+            // consume // comments
+            while (ch = this.getch()) {
+              if (ch === "\n") {
+                this.nextch = ch;
+                break;
+              }
+            }
+            if (ch) return this.next();
+            token = '';
+            type = EOF;
+          } else if (sstc && nextch === '*') {
+            // consume /* ... */ comments
+            while (ch = this.getch()) {
+              if (ch === "*") {
+                ch = this.getch();
+                if (ch === '/') {
+                  break;
+                }
+              }
+              if (ch === "\n") this.lineno++;
+            }
+            if (ch) return this.next();
+            token = '';
+            type = EOF;
+          }
+        } else {
+          token = ch;
+          type = SYMBOL;
+        }
+        break;
+      }
+      ch = this.getch();
+    }
+    this.token = token;
+    this.type = type;
+    this.nextch = nextch;
+    if (type === NUMBER) {
+      token = parseFloat(token);
+    }
+    return { token, type, nextch, lineno: this.lineno };
+  }
 }
 
 class StringTokeniser extends Tokeniser {
-	constructor(string, options) {
-		super(options);
-		this.string = string;
-		this.pos = 0;
-	}
-	get() {
-		return this.string[this.pos++];
-	}
+  constructor(string, options) {
+    super(options);
+    this.string = string;
+    this.pos = 0;
+  }
+  get() {
+    return this.string[this.pos++];
+  }
 }
 
 StringTokeniser.TT_EOF = Tokeniser.TT_EOF = EOF;
