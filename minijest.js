ac091e6fffe938d5fee153641421b702f9645369
Mehuge <adf@sorcerer.co.uk>
2018-02-13 23:30:42 +0000

    Set indentation to 2 spaces

--- minijest.js
+++ minijest.js
@@ -2,21 +2,21 @@
 const state = {};
 
 async function test(n,f) {
-	state.name = n;
-	try {
-		console.log(`Running test ${n}`);
-		f();
-	} catch(e) {
-		console.dir(e);
-	}
+  state.name = n;
+  try {
+    console.log(`Running test ${n}`);
+    f();
+  } catch(e) {
+    console.dir(e);
+  }
 }
 
 function expect(v) {
-	return {
-		toBe: function(t) {
-			if (v !== t) throw new Error(`${state.name} test failed. Expected [${v}] to be [${t}]`);
-		}
-	}
+  return {
+    toBe: function(t) {
+      if (v !== t) throw new Error(`${state.name} test failed. Expected [${v}] to be [${t}]`);
+    }
+  }
 }
 
 module.exports = { test, expect };
