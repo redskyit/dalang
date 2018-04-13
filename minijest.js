fe1b3a8e6679c73f6f052c9d89fe6b4dac810637
Mehuge <adf@sorcerer.co.uk>
2018-04-13 13:32:57 +0100

    Add Jest.dont().expect() support for negated tests

--- minijest.js
+++ minijest.js
@@ -5,27 +5,47 @@ async function test(n,f) {
   state.name = n;
   try {
     console.log(`Running test ${n}`);
-    f();
+    await f();
   } catch(e) {
     console.dir(e);
   }
 }
 
 function expect(v) {
+  const dont = this._dont;
+  let t;
   return {
     toBe: function(t) {
-      if (v !== t) {
-        throw new Error(`${state.name} test failed. Expected [${t}] got [${v}]`);
+      t = v === t;
+      if (dont) {
+        if (t) throw new Error(`${state.name} test failed. Didn't expect [${v}] to be [${t}]`);
+      } else {
+        if (!t) throw new Error(`${state.name} test failed. Expected [${v}] to be [${t}]`);
       }
     },
     toEqual: function(t) {
-      if (v != t) {
-        throw new Error(`${state.name} test failed. Expected [${t}] got [${v}]`);
+      t = v == t;
+      if (dont) {
+        if (t) throw new Error(`${state.name} test failed. Didn't expected [${v}] to equal [${t}]`);
+      } else {
+        if (!t) throw new Error(`${state.name} test failed. Expected [${v}] to equal [${t}]`);
+      }
+    },
+    toBeInRange: function(l,h) {
+      t = v >= l && v <= h;
+      if (dont) {
+        if (t) throw new Error(`${state.name} test failed. Didn't expect [${v}] to be in range [${l}:${h}]`);
+      } else {
+        if (!t) throw new Error(`${state.name} test failed. Expected [${v}] to be in range [${l}:${h}]`);
       }
     }
   }
 }
 
-module.exports = { test, expect };
+function dont() {
+  return { _dont: true, expect: expect, dont: function() { return module.exports; } };
+}
+
+module.exports = { test, expect, dont };
 
 /* vim: set ai ts=2 sw=2 expandtab smarttab softtabstop=2 : */
