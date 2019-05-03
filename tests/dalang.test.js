e309294c17e0ae6f6c2599f334db355d8523b19c
Mehuge <adf@sorcerer.co.uk>
2019-05-03 18:27:30 +0100

    Update jest to version 24.7.1
    closes

--- tests/dalang.test.js
+++ tests/dalang.test.js
@@ -1,7 +1,7 @@
 test('Instanciation', async () => {
-	const dalang = require('./dalang');
+	const dalang = require('../dalang');
 });
 
 test('Custom dalang instansiation', async () => {
-	const dalang = new (require('./dalang').Dalang)({ defaultTimeout: 60 });
+	const dalang = new (require('../dalang').Dalang)({ defaultTimeout: 60 });
 });
--- tests/parser.test.js
+++ tests/parser.test.js
@@ -2,8 +2,9 @@ const dalang = require('../dalang');
 // test('Parser', async () => {
 //	await dalang.run('parser.test');
 // });
-(async function() { 
-	await dalang.run('parser.test', { headless: false, 
+test('Parser', async () => { 
+	await dalang.run('tests/parser.test', {
+		headless: false, 
 		executablePath: process.env.CHROME_PATH
 	}); 
-})();
+}, 30000);
